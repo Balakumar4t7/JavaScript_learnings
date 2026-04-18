e4e56ec490c13fa26993e66c8fbc205d1bbdfbb7
Balakumar4t7 <balakumarbkt2005@gmail.com>
2026-04-18 21:12:38 +0530

    added simple array method programs

--- Array_basics.js
+++ Array_basics.js
@@ -1,9 +1,21 @@
-const num=[1,2,3,4,5,6,7,8,9,10];
-console.log(num.length);
-let result=num.filter(n => n >5);
+const nums= [3, 7, 2, 9, 4, 11, 6, 8, 1, 5]; 
+ 
+let nums_Greater = nums.filter(n => n>5); // used filter method to get values greater than 5
+console.log(nums_Greater);  // DIsplaying the result
 
-console.log(result.length);
-for(let i=0;i<num.length;i++){
-    console.log(num[i]);
-    }
+let sq_nums= nums.map(n => n*n); // used map to create an new array which is a square of the nums Array
+console.log(sq_nums);
+
+nums.forEach(n => console.log("Number : " + n)); // using for each method to print the numbers
+
+let num= nums.find(n => n>8) // used find() method to find the next number greater than 8
+console.log(num);
+
+const names = ["Bala", "Gopal" , "Zoro","Luffy"];
+names.forEach(n => console.log(n+" is a Developer"));
+let names_dev= names.map(n => n+ " is a Developer");
+console.log(names_dev);
+
+
+
 
