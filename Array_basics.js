b56e2dac48684537fe8676a41ea7ab2cf8d1a217
Balakumar4t7 <balakumarbkt2005@gmail.com>
2026-04-18 20:37:41 +0530

    basic Array Practice programs

---
+++ Array_basics.js
@@ -0,0 +1,9 @@
+const num=[1,2,3,4,5,6,7,8,9,10];
+console.log(num.length);
+let result=num.filter(n => n >5);
+
+console.log(result.length);
+for(let i=0;i<num.length;i++){
+    console.log(num[i]);
+    }
+
